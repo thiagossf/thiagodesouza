5d3afe07c59f6e00e75f94289225f1bd2b11c559
thiagossf <thiagosouza887@gmail.com>
2025-06-24 11:38:34 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('botaoMensagem').addEventListener('click', function() {
+    document.getElementById('mensagem').textContent = "Você é capaz de conquistar o que quiser. Continue se dedicando!";
+});
